568b3d35868b3b7006fef8b899fda6bd6b6e9c38
Iracopter <114257500+Iracopter@users.noreply.github.com>
2023-02-24 22:52:48 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const num1=Number(prompt('Enter first number'));
+const num2=Number(prompt('Enter second number'));
+
+alert(`${num1} + ${num2} = ${num1+num2}
+${num1} - ${num2} = ${num1-num2} 
+${num1} * ${num2} = ${num1*num2}
+${num1} / ${num2} = ${num1/num2}`);
