aedb1cbd620eea2f24c5d9dd693e2e2f9f1b92aa
Iracopter <114257500+Iracopter@users.noreply.github.com>
2023-03-06 21:18:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
-const num1=Number(prompt('Enter first number'));
-const num2=Number(prompt('Enter second number'));
+const number1=(prompt("Enter first number"));
+const number2=(prompt("Enter second number"));
 
-alert(`${num1} + ${num2} = ${num1+num2}
+if(number1===null || number1==='' || number2===null || number2===''){
+    alert("Ви не ввели число, спробуйте ще раз!");
+}
+else{
+    let num1=Number(number1);
+    let num2=Number(number2);
+    if(num2!=0){
+        if(num1<num2){
+            if(confirm("Ви впевнені, що хочете продовжити операцію?")==true){
+                alert(`${num1} + ${num2} = ${num1+num2}
 ${num1} - ${num2} = ${num1-num2} 
 ${num1} * ${num2} = ${num1*num2}
 ${num1} / ${num2} = ${num1/num2}`);
+            }
+        }
+        else{
+            alert(`${num1} + ${num2} = ${num1+num2}
+${num1} - ${num2} = ${num1-num2} 
+${num1} * ${num2} = ${num1*num2}
+${num1} / ${num2} = ${num1/num2}`);
+        }
+    }
+    else{
+        alert(`${num1} + ${num2} = ${num1+num2}
+${num1} - ${num2} = ${num1-num2} 
+${num1} * ${num2} = ${num1*num2}
+${num1} / ${num2} = На нуль ділити не можна`);  
+    }
+}
